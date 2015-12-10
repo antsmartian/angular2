00d5f468d34eea160a0d7581707e24cc86b98fbc
Ants <anto.aravinth.cse@gmail.com>
2015-12-10 17:37:23 +0530

    designedNavBar

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,13 +1,29 @@
 import {Component, View} from 'angular2/angular2';
-import {RouteConfig, Router, RouterOutlet, RouterLink} from 'angular2/router';
 
 @Component({
     selector: 'app'
 })
 @View({
-    directives: [RouterOutlet, RouterLink],
     template: `
-       <h1>Hello World</h1>
+  <div>
+      <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">Udemy Course Tracker</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Dashboard</a></li>
+               <li><a>Add/Delete</a></li>
+              </ul>
+            </div>
+          </div>
+      </nav>
+
+      <div>
+        <router-outlet></router-outlet>
+      </div>
+  </div>
   `
 })
 export class App {

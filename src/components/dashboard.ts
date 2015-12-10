4678f75548a98c0a0e1590d5ff2bfb49c4786e3c
Ants <anto.aravinth.cse@gmail.com>
2015-12-10 17:54:31 +0530

    Fixed the view issue on dashboard component

--- src/components/dashboard.ts
+++ src/components/dashboard.ts
@@ -1,7 +1,10 @@
 import {Component, View} from 'angular2/angular2';
 
 @Component({
-    selector: 'manage'
+    selector: 'manage',
+    template: `
+        <h1>Rendering dashboard </h1>
+    `
 })
 export class Dashboard {
 

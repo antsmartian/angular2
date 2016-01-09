3651278c6cd2ea662e33d27f02a2d8aad6d0b60d
Ants <anto.aravinth.cse@gmail.com>
2016-01-09 11:31:09 +0530

    Removing the view annotataion as its deprecated

--- src/components/card.ts
+++ src/components/card.ts
@@ -1,10 +1,8 @@
-import {Component, View , Input} from 'angular2/core';
+import {Component , Input} from 'angular2/core';
 
 @Component({
     selector: 'Card',
     properties: ['course: course']
-})
-@View({
     template: `
     <div style="box-shadow: 10px 10px 5px #888888;width: 400px;height: 150px; background-color : #ebffc6 ; padding: 25px; ">
         <b>{{course.title}}</b>
